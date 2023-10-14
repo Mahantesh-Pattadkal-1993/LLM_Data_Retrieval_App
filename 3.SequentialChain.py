80f933f4c79f58a0c904205b2138f1a304dfc162
Mahantesh Pattadkal <mpattadkal@gmail.com>
2023-10-14 15:35:39 +0200

    Streamlit App with response from LLM

---
+++ 3.SequentialChain.py
@@ -0,0 +1,53 @@
+import os
+from Credentials import api_key
+os.environ["OPENAI_API_KEY"] = api_key
+
+from langchain.llms import OpenAI 
+llm = OpenAI(temperature = 0.7 )
+
+
+"""
+Build two chains and use Sequential Chain functions
+
+"""
+
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+
+#------------------------------------------------------------
+# Chain 1 -  Identify Tourist Place
+#------------------------------------------------------------
+
+prompt_TouristPlace = PromptTemplate(
+
+    input_variables= ["Country"],
+    template=  "Suggest me one famous tourist place from {Country}"
+)
+
+TouristPlace_chain = LLMChain(llm=llm, prompt=prompt_TouristPlace, output_key="TouristPlace")
+
+#------------------------------------------------
+# Chain 2 -  Generate Itinerary
+#------------------------------------------------
+
+
+prompt_Itinerary = PromptTemplate(
+
+    input_variables= ["TouristPlace"],
+    template=  "Create an itinerary to reach {TouristPlace} from Mumbai and specify means of transport, return the ouput as commma separated list "
+)
+
+Itinerary_chain = LLMChain(llm=llm, prompt=prompt_Itinerary, output_key="Itinerary")
+
+# Combine both the chains
+
+from langchain.chains import SequentialChain
+
+final_chain = SequentialChain(
+    chains = [TouristPlace_chain,Itinerary_chain],
+    input_variables= ["Country"],
+    output_variables= ["TouristPlace","Itinerary"]
+)
+
+response = final_chain({"Country":"Egypt"})  # here as the sequential chain can have multiple input, we use dictionary to send inputs
+print(response)
---
+++ app.py
@@ -0,0 +1,19 @@
+import streamlit as st
+from app_helper import generate_response 
+
+# Title of the App
+st.title("Tourist name Generator")
+
+# Sidebar with Choice for Country
+Country = st.sidebar.selectbox("Pick Country of your choice", ("Brazil", "France","Italy","Switzerlnd", "England"))
+
+# Show Response
+if Country:
+    response = generate_response(CountryOfChoice=Country) # get response
+    st.header(response["TouristPlace"].strip()) # show "tourist place" on the app page under title
+    
+    itinerary = response["Itinerary"].strip().split(",") # show itineraries on the app page under title  
+    st.write("***Itinerary***")
+
+    for item in itinerary:
+        st.write("- ", item)
